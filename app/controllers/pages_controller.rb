d7d9bc2c4b727c50c6da1ae5495eae7dd7b1c17c
Eric Lee <eric.wonsuk.lee@gmail.com>
2014-02-14 12:01:32 -0600

    Starting pt

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,24 @@
+class PagesController < ApplicationController #separate by methods/domain model. self organization purpose
+
+  def home #definng a method
+    # @description =
+  end
+
+  def newexp
+  end
+
+  def index
+    @payment_list = ["Venmo", "Paypal", "SquareCash"]
+  end
+
+  def show
+  end
+
+  def signin
+  end
+
+  def newflightinfo
+  end
+
+
+end
